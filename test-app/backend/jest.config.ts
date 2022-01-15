d614401b38244d2a70e1ee232f56601d21426fc2
J3T4R0 <niemiryan@gmail.com>
2022-01-15 15:22:24 -0800

    [Fix] suggested PR changes

--- test-app/backend/jest.config.ts
+++ test-app/backend/jest.config.ts
@@ -1,12 +1,12 @@
-import type {Config} from '@jest/types';
+import type {Config} from '@jest/types'
 // Sync object
-const config: Config.InitialOptions = {
-  preset: "ts-jest",
-  testEnvironment: "node",  
+const config: Config.InitialOptions = { 
+  preset: 'ts-jest',
+  testEnvironment: 'node',  
   verbose: true,
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-  },
-};
+  transform: { 
+    '^.+\\.ts?$': 'ts-jest'
+  }
+ }
 
-export default config;
+export default config
