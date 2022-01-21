b202d8a7b3b4c81cde35f9e2146c463a5e901fc5
Kevin Sittner <kevinsittner6@gmail.com>
2022-01-21 00:45:48 -0800

    Added redirect from /home to /

--- test-app/frontend/src/App/App.tsx
+++ test-app/frontend/src/App/App.tsx
@@ -1,15 +1,16 @@
 import Home from '../Home/Home';
 import Login from '../Login/Login';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
       <BrowserRouter>
          <Routes>
             <Route path="/" element={<Home/>} />
+            <Route path="/home" element={<Navigate replace to="/" />} />
             <Route path="/login" element={<Login/>} />
-            <Route path="*" element={<div>404</div>} />
+            <Route path="*" element={<p>404</p>} />
          </Routes>
       </BrowserRouter>
   );
