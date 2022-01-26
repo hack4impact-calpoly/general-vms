f06aa8c548c93ef16992e46eae0964ec23c705ac
bglossner <benglossner@gmail.com>
2022-01-26 13:37:42 -0800

    fix: Try to make everything more compatible with windows

--- test-app/scripts/install-husky.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel/246128#246128
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-APP_DIR="$SCRIPT_DIR/.."
-
-cd "$(git rev-parse --show-toplevel)" && npx husky install "$APP_DIR/.husky"
