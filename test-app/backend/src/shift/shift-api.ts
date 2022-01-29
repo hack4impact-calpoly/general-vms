a7709560d2e6cc04b5ecc1060c373c269f6fb2f4
Pandoyer <adam@mezanet.com>
2022-01-28 19:23:58 -0800

    fixed most the lint issues

--- test-app/backend/src/shift/shift-api.ts
+++ test-app/backend/src/shift/shift-api.ts
@@ -1,42 +1,42 @@
-import shift from '/shift-interface';
-import app from '../server'; 
-import isUserAdmin from "../middleware";
+import app from '../server';
+import { isUserAdmin } from '../middleware';
+import Database from '../database';
+import shift from './shift-interface';
 
 app.post('/api/new-shift', isUserAdmin, async (req, res) => {
-    console.log("POST: Creating Calendar Event...");
+  console.log('POST: Creating Calendar Event...');
 
-    const { start, end, maxVolunteers, title, description,  eventAdmin} = req.body;
+  const { start, end, maxVolunteers, title, description, eventAdmin } = req.body as shift;
 
-    if (!title) {
-        res.status(403).json({
-            message: 'Must include title',
-        });    return;
-    }
+  if (!title) {
+    res.status(403).json({
+      message: 'Must include title',
+    }); return;
+  }
 
-    if (start >= end){
-        res.status(403).json({
-            message: 'Start date must come before end date',
-        });    return;
-    }
+  if (start >= end) {
+    res.status(403).json({
+      message: 'Start date must come before end date',
+    }); return;
+  }
 
-    if(maxVolunteers <= 0){
-        res.status(403).json({
-            message: 'Max Volunteers must be greater than 0',
-        });    return;
-    }
+  if (maxVolunteers <= 0) {
+    res.status(403).json({
+      message: 'Max Volunteers must be greater than 0',
+    }); return;
+  }
 
-    const newShift = {
+  const newShift = {
     start: start,
     end: end,
     maxVolunteers: maxVolunteers,
     title: title,
     description: description,
     eventAdmin: eventAdmin,
-    };
-    const eventCreated = await Database.saveShift(newShift);
+  };
+  await Database.saveShift(newShift);
 
-    res.status(201).json({
-        message: 'New shift successfully created',
-    });    return;
-
+  res.status(201).json({
+    message: 'New shift successfully created',
   });
+});
--- test-app/backend/src/shift/shift-interface.ts
+++ test-app/backend/src/shift/shift-interface.ts
@@ -1,9 +1,11 @@
-export interface shift {
-    start: Date,
-    end: Date,
-    maxVolunteers: Number,
-    title: String,
-    description: String,
-    eventAdmin: String,
-    getShiftTime(): Number
+interface shift {
+  start: Date,
+  end: Date,
+  maxVolunteers: number,
+  title: string,
+  description: string,
+  eventAdmin: string,
+  getShiftTime(): number
 }
+
+export default shift;
