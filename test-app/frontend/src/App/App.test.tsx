4301b4b6a9eb108f5d6cb9e1977991719a91164f
Khoa Ly <kly27@calpoly.edu>
2022-02-05 14:27:07 -0800

    Fixed error with routing

--- test-app/frontend/src/App/App.test.tsx
+++ test-app/frontend/src/App/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('defualt page should be home page', () => {
+test('default page should be home page', () => {
   render(<App />);
   const homeEle = screen.getByText(/home/i);
   expect(homeEle).toBeInTheDocument();
