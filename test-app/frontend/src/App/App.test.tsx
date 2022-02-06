61123c590593c99a42a63959e7f0e7c1557f7baf
Khoa Ly <kly27@calpoly.edu>
2022-02-06 01:12:56 -0800

    Added original teset

--- test-app/frontend/src/App/App.test.tsx
+++ test-app/frontend/src/App/App.test.tsx
@@ -1,6 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// Original Test would not pass
+// test('default page should be home page', () => {
+//   render(<App />);
+//   const homeEle = screen.getByText(/home/i);
+//   expect(homeEle).toBeInTheDocument();
+// });
+
 test('default page should be home page', () => {
   render(<App />);
   const homeEle = screen.getAllByText(/home/i);
