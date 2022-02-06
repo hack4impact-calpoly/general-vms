4c1c9ebfec6b2c30d448ddc7e7f1ba80f6d5da9b
Khoa Ly <kly27@calpoly.edu>
2022-02-06 00:47:24 -0800

    Edited App.test.tsx file

--- test-app/frontend/src/App/App.test.tsx
+++ test-app/frontend/src/App/App.test.tsx
@@ -4,5 +4,5 @@ import App from './App';
 test('default page should be home page', () => {
   render(<App />);
   const homeEle = screen.getAllByText(/home/i);
-  expect(homeEle).toBeInTheDocument();
+  expect(homeEle).toHaveLength(2);
 });
