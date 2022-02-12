c2d6caf1c77464d16fe1d664724d206cc38d5e45
Khoa Ly <kly27@calpoly.edu>
2022-02-12 13:30:44 -0800

    Fixed image display

--- test-app/frontend/src/Navbar/Navbar.tsx
+++ test-app/frontend/src/Navbar/Navbar.tsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
+import h4ilogo from '../assets/images/logo192.png';
 import './Navbar.scss';
 
 function NavBar() {
   return (
     <header className="main-header">
       <p className="main-text">
-        <Link to="/"><img className="main-logo" src='../assets/images/logo192.png' alt="Org Logo" /></Link>
+        <Link to="/"><img className="main-logo" src={h4ilogo} alt="Org Logo" /></Link>
         <Link to="/">General VMS</Link>
       </p>
       <nav className="navbar">
